abf7a6b0fb6dc72f1176efce7ebd02d90dbe13a5
Ariana <arianawalcott1517@gmail.com>
2023-06-26 10:18:19 -0400

    Initial Commit

---
+++ PRACTICE_2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  PRACTICE_2
+//
+//  Created by Antonio Walcott on 6/26/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PRACTICE_2/PRACTICE_2App.swift
@@ -0,0 +1,17 @@
+//
+//  PRACTICE_2App.swift
+//  PRACTICE_2
+//
+//  Created by Antonio Walcott on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct PRACTICE_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
